8340bbc7b3ccdd21972784bf2711bbbeea7d6b86
Brian <okellobrian18@gmail.com>
2025-06-18 14:57:47 +0300

    cr lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -14,14 +14,40 @@ app.json.compact = True
 migrate = Migrate(app, db)
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+    
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plant_list = [plant.to_dict() for plant in plants]
+        return make_response(jsonify(plant_list), 200)
+
+    def post(self):
+        data = request.get_json()
+        try:
+            new_plant = Plant(
+                name=data['name'],
+                image=data['image'],
+                price=data['price']
+            )
+            db.session.add(new_plant)
+            db.session.commit()
+            return make_response(new_plant.to_dict(), 201)
+        except Exception as e:
+            return make_response({'error': str(e)}, 400)
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if plant:
+            return make_response(plant.to_dict(), 200)
+        return make_response({'error': 'Plant not found'}, 404)
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
